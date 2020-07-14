0c44aac2aaaec7bbb81195137d082cfd73d06a53
Luis Bustamante <bustamante.luis@yahoo.com>
2020-07-14 21:21:57 +0200

    language redirection

---
+++ js/language.js
@@ -0,0 +1,17 @@
+let languages = new Map()
+languages.set('de', './de/')
+languages.set('en', './en/')
+var userLang = navigator.language || navigator.userLanguage 
+if(userLang.length >= 2){
+    var lang = userLang.substr(0,2)
+    if(languages.has(lang)){
+        window.location.replace(languages.get(lang))
+    }else{
+        window.location.replace(languages.get('de'))    
+    }
+}else{
+    window.location.replace(languages.get('de'))
+}
+
+
+
